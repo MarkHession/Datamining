ba6785d3011e1706dea08b2f79da932c5f84de97
Mark <a00239393@student.ait.ie>
2019-11-18 17:08:14 +0000

    Bits

--- Regression.R
+++ Regression.R
@@ -4,3 +4,9 @@ library('ggplot2')
 machine <- read.csv("data/machine.csv")
 
 head(machine)
+data(machine)
+
+plot(machine$PRP, machine$ERP)
+pairs(machine)
+
+model = lm(CACH ~ CHMIN + CHMAX + PRP., data=machine)
