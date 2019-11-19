e9b77bf4a89eab5d29d4fc5041e8fa0449000078
Mark <a00239393@student.ait.ie>
2019-11-19 16:33:46 +0000

    Did bits on report

--- Regression.R
+++ Regression.R
@@ -5,19 +5,29 @@ machine <- read.csv("data/machine.csv")
 
 machine$Model.Name=NULL
 machine$ï..Vendor.Name= NULL
+machine$MYCT= NULL
+machine$MMIN= NULL
+machine$MMAX= NULL
 
 
+pairs(machine)
 head(machine)
 data(machine)
-pairs(machine)
 
 
 
-machine
+#model generation
 machine.lm= lm(ERP ~ CACH + CHMIN + CHMAX + PRP, data=machine)
 summary(machine.lm)
 machine.lm$coefficients
 
+#prediction
 newdata1 = data.frame( CACH= 128, CHMIN=8, CHMAX=64, PRP=367)
 newdata1
 predict(machine.lm,newdata1)
+
+# using values from dataset to compare accuracy
+dataCheck = data.frame( CACH= 256, CHMIN=16, CHMAX=128, PRP=198)
+dataCheck
+predict(machine.lm,dataCheck)
+#prediction is 196, dataset is 199, within 90% accuracy
